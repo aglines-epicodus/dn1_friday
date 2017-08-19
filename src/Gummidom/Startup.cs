248d7503728c8da17ea8bdfde4676b2055190ad1
Andrew Glines <aglines-epicodus@gmail.com>
2017-08-19 16:48:18 -0700

    Added bootstrap and some styling

--- src/Gummidom/Startup.cs
+++ src/Gummidom/Startup.cs
@@ -53,7 +53,7 @@ namespace Gummidom
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Products}/{action=Index}/{id?}");
             });
 
 
